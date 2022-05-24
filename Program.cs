e5e79ceb353dac94a7947d6d93d164372f86e3ef
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-24 13:39:15 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace For_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 10; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (var i = 10; i  >= 1; i--)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
